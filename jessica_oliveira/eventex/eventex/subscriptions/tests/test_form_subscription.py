dca554742932eb0a8310fed429d27b44f3a2be99
Jessica Oliveira <jessica.oliveira@instruct.com.br>
2019-10-29 15:11:59 -0300

    Atualização eventex módulo 2

---
+++ jessica_oliveira/eventex/eventex/subscriptions/tests/test_form_subscription.py
@@ -0,0 +1,12 @@
+from django.test import TestCase
+from eventex.subscriptions.forms import SubscriptionForm
+
+class SubscriptionFormTest(TestCase):
+    def setUp(self):
+        self.form = SubscriptionForm()
+
+
+    def test_has_fields(self):
+        """Form must have fields"""
+        expected = ['name', 'cpf', 'email', 'phone']
+        self.assertSequenceEqual(expected, list(self.form.fields))
